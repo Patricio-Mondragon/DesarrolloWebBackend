7053f6ded88edfdd81dc5f0c82ba1ebc893a75cc
Patricio Mondragon <a01748352@itesm.mx>
2022-04-28 16:31:08 -0500

    Primer Login

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import email
+from urllib import request
+from flask import Flask, render_template, request
 
 # FlASK
 #############################################################
@@ -9,7 +11,44 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/prueba')
-def prueba():
-    return "Esta es una pagina prueba"
 
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    return render_template("Login.html", error = email)
+
+
+@app.route("/loginuser", methods = ["POST"])
+def loginuser():
+    email = request.form["email"]
+    password = request.form["password"]
+    return render_template("index.html", error = email)
+
+@app.route('/prueba', methods=["GET"])
+def prueba():
+    nombres = []
+    nombres.append({"nombre": "ruben",
+
+    "Semestre01":[{
+        "matematicas":"10",
+        "español":"5"
+        }],
+    "Semestre02":[{
+        "matematicas":"10",
+        "español":"5"
+        }]
+        })
+
+    nombres.append({"nombre": "sergio",
+
+    "Semestre021":[{"matematicas":"8",
+    "español":"6"
+    }],
+    "Semestre02":[{
+        "matematicas":"4",
+        "español":"9"
+    }]
+    })
+
+    return render_template("home.html", data = nombres)
+
+    
